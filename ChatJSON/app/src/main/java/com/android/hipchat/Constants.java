18ba7ba66fe0a0690f5a988538e4e7df89d118c2
nanditha.gangappa <nanditha.gangappa@lge.com>
2017-02-27 14:35:19 +0530

    InitialCode
    Android Take Home Test

---
+++ ChatJSON/app/src/main/java/com/android/hipchat/Constants.java
@@ -0,0 +1,15 @@
+package com.android.hipchat;
+
+/**
+ * Created by nanditha.gangappa on 2/24/2017.
+ */
+public class Constants {
+    public static final short SET_RESULT = 1;
+
+    public static final String sJsonLinks = "links";
+    public static final String sJsonURL = "url";
+    public static final String sJsonTitle = "title";
+
+    public static final String sJsonEmotIcons = "emoticons";
+    public static String  sJsonString = "mentions";
+}
